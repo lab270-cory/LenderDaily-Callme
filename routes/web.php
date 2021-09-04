fbc8e31d25cd1be2d0b5ad5d3139fb3fd8f3aaa4
avinash kumar <ak403602@gmail.com>
2021-09-04 06:14:07 +0530

    Test Code for call

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,50 @@ Route::group(['middleware' => ['auth', 'verified']], function (){
    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
    Route::resource('users', \App\Http\Controllers\UserController::class);
 });
+
+
+Route::post('/call', function (\Illuminate\Http\Request $request) {
+    // Get form input
+    $userPhone = $request->input('userPhone');
+    $encodedSalesPhone = urlencode(str_replace(' ','',$request->input('salesPhone')));
+    // Set URL for outbound call - this should be your public server URL
+    $host = parse_url(Request::url(), PHP_URL_HOST);
+
+    // Create authenticated REST client using account credentials in
+    // <project root dir>/.env.php
+    $client = new Twilio\Rest\Client(
+        getenv('TWILIO_ACCOUNT_SID'),
+        getenv('TWILIO_AUTH_TOKEN')
+    );
+
+    try {
+        $client->calls->create(
+            $userPhone, // The visitor's phone number
+            getenv('TWILIO_NUMBER'), // A Twilio number in your account
+            array(
+                "url" => "http://$host/outbound/$encodedSalesPhone"
+            )
+        );
+    } catch (Exception $e) {
+        // Failed calls will throw
+        return $e;
+    }
+
+    // return a JSON response
+    return array('message' => 'Call incoming!');
+});
+
+// POST URL to handle form submission and make outbound call
+Route::post('/outbound/{salesPhone}', function ($salesPhone) {
+    // A message for Twilio's TTS engine to repeat
+    $sayMessage = 'Thanks for contacting our sales department. Our
+        next available representative will take your call.';
+
+    $twiml = new \Twilio\TwiML\VoiceResponse();
+    $twiml->say($sayMessage, array('voice' => 'alice'));
+    $twiml->dial($salesPhone);
+
+    $response = Response::make($twiml, 200);
+    $response->header('Content-Type', 'text/xml');
+    return $response;
+});
